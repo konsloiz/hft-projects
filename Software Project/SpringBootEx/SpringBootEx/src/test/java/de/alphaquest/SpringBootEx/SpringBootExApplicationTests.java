ffde484bea13637b4e908efd8f859df9f66dc452
Konstantinos Loizas <konsloiz@gmail.com>
2020-11-18 19:20:18 +0100

    Test Home

--- Software Project/SpringBootEx/SpringBootEx/src/test/java/de/alphaquest/SpringBootEx/SpringBootExApplicationTests.java
+++ Software Project/SpringBootEx/SpringBootEx/src/test/java/de/alphaquest/SpringBootEx/SpringBootExApplicationTests.java
@@ -1,13 +1,28 @@
 package de.alphaquest.SpringBootEx;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import de.alphaquest.SpringBootEx.Controller.ApplicationController;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringBootExApplicationTests {
 
+	ApplicationController applicationController = new ApplicationController();
+	
 	@Test
 	void contextLoads() {
+	}
+
+
+	@Test
+	public void testHome(){
+		String result = applicationController.home();
+		assertEquals(result,"Hello world! This is a Spring Boot application made for alphaQuest project");
 	}
 
 }
