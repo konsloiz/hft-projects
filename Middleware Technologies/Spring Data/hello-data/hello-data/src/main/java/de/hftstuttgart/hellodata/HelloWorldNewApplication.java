219416173aade5c88373c61fc6a246431b97753f
Konstantinos Loizas <konsloiz@gmail.com>
2020-12-06 19:34:39 +0100

    Added app-profiles

---
+++ Middleware Technologies/Spring Data/hello-data/hello-data/src/main/java/de/hftstuttgart/hellodata/HelloWorldNewApplication.java
@@ -0,0 +1,14 @@
+package de.hftstuttgart.hellodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HelloWorldNewApplication {
+
+ 	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldNewApplication.class, args);
+	}
+
+}
