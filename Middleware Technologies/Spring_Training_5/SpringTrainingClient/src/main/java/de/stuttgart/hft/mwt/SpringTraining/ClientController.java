95784a8372056d164cbcc005e20205b9539ca46f
konsloiz <konsloiz@gmail.com>
2021-01-05 11:38:20 +0100

    Module [06]: Synchronous Communication, API & REST

---
+++ Middleware Technologies/Spring_Training_5/SpringTrainingClient/src/main/java/de/stuttgart/hft/mwt/SpringTraining/ClientController.java
@@ -0,0 +1,21 @@
+package de.stuttgart.hft.mwt.SpringTraining;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RefreshScope
+public class ClientController {
+
+	@Value("${message:Hello default}")
+	private String message;
+
+	@RequestMapping ("/message")
+	String getMessage() {
+		return this.message;
+	}
+}
