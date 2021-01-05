b5bde9c007e5d64f0cf3a6220263357891b07e1e
Konstantinos Loizas <konsloiz@gmail.com>
2021-01-05 17:46:06 +0100

    Module [05]: Configuration & Profiles

---
+++ Middleware Technologies/MWT_Training_5/SpringTrainingClient/src/test/java/de/stuttgart/hft/mwt/SpringTraining/SpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stuttgart.hft.mwt.SpringTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
